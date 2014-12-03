14bf0337089ff037fe55b68513b401675d4311ca
MichaelTong <tonghao1992@gmail.com>
2014-12-02 19:22:18 -0600

    no record every byte

--- main.c
+++ main.c
@@ -98,6 +98,8 @@ void cleanup();
 void params(int argc, char* argv[]);
 void print_params_err(int state, int err);
 char* makefilename();
+void analyze_mem_layout(UINT64 begin_a, UINT64 size_m, int map_scheme);
+void address_mapping(UINT64 physicalAddress, int *chan, int *rank, int *bank, int *row, int *col, int addressMappingScheme);
 
 int main(int argc, char* argv[])
 {
@@ -300,12 +302,12 @@ void reading(bool sim, int read_mode)
         {
             ptr = base + i*read_interval;
 
-            clock_gettime(CLOCK_REALTIME, &srts_b);
+            //clock_gettime(CLOCK_REALTIME, &srts_b);
             reg = *(ptr);
-            clock_gettime(CLOCK_REALTIME, &srts_e);
+            //clock_gettime(CLOCK_REALTIME, &srts_e);
 
-            run_time = (srts_e.tv_sec-srts_b.tv_sec)*1000000000 + srts_e.tv_nsec-srts_b.tv_nsec;
-            timeRecord[i]+=run_time;
+            //run_time = (srts_e.tv_sec-srts_b.tv_sec)*1000000000 + srts_e.tv_nsec-srts_b.tv_nsec;
+            //timeRecord[i]+=run_time;
         }
     }
     clock_gettime(CLOCK_REALTIME, &rts_e);
@@ -314,7 +316,7 @@ void reading(bool sim, int read_mode)
     run_time = (rts_e.tv_sec-rts_b.tv_sec)*1000000000 + rts_e.tv_nsec-rts_b.tv_nsec;
     print_info(CL_READING_E);
 
-    statistics(read_size,makefilename());
+    statistics(0/**read_size**/,makefilename());
     return;
 }
 
@@ -647,4 +649,281 @@ void init(int argc, char* argv[])
     memarray = (BYTE *)malloc(totalBytes*sizeof(BYTE));
     printf("Address of Memarray:\t %llx\n",(UINT64)memarray);
     return;
+}
+#define NUM_CHANS_LOG 1
+#define NUM_RANKS_LOG 1
+#define NUM_BANKS_LOG 3
+#define NUM_ROWS_LOG 13
+#define NUM_COLS_LOG 10
+#define BYTE_OFFSET_WIDTH 3
+#define TRANSACTION_SIZE 64
+#define COL_LOW_BIT_WIDTH 3
+unsigned channelBitWidth = NUM_CHANS_LOG;
+unsigned rankBitWidth = NUM_RANKS_LOG;
+unsigned bankBitWidth = NUM_BANKS_LOG;
+unsigned rowBitWidth = NUM_ROWS_LOG;
+unsigned colBitWidth = NUM_COLS_LOG;
+unsigned colLowBitWidth = COL_LOW_BIT_WIDTH;
+unsigned colHighBitWidth;
+
+
+void analyze_mem_layout(UINT64 begin_a, UINT64 size_m, int map_scheme)
+{
+	UINT64 tempA, tempB, end_a;
+	unsigned transactionSize = TRANSACTION_SIZE;
+	UINT64 transactionMask =  transactionSize - 1; //ex: (64 bit bus width) x (8 Burst Length) - 1 = 64 bytes - 1 = 63 = 0x3f mask
+	// this forces the alignment to the width of a single burst (64 bits = 8 bytes = 3 address bits for DDR parts)
+	unsigned byteOffsetWidth = BYTE_OFFSET_WIDTH;
+	// Since we're assuming that a request is for BL*BUS_WIDTH, the bottom bits
+	// of this address *should* be all zeros if it's not, issue a warning
+    colHighBitWidth = colBitWidth - colLowBitWidth;
+	if ((begin_a & transactionMask) != 0)
+	{
+		printf("WARNING: address 0x%llx is not aligned to the request size of %d.\n", begin_a,transactionSize);
+	}
+
+	// each burst will contain JEDEC_DATA_BUS_BITS/8 bytes of data, so the bottom bits (3 bits for a single channel DDR system) are
+	// 	thrown away before mapping the other bits
+	end_a = begin_a + size_m -1;
+	begin_a >>= byteOffsetWidth;
+	end_a >>= byteOffsetWidth;
+
+
+	// The next thing we have to consider is that when a request is made for a
+	// we've taken into account the granulaity of a single burst by shifting
+	// off the bottom 3 bits, but a transaction has to take into account the
+	// burst length (i.e. the requests will be aligned to cache line sizes which
+	// should be equal to transactionSize above).
+	//
+	// Since the column address increments internally on bursts, the bottom n
+	// bits of the column (colLow) have to be zero in order to account for the
+	// total size of the transaction. These n bits should be shifted off the
+	// address and also subtracted from the total column width.
+	//
+	// I am having a hard time explaining the reasoning here, but it comes down
+	// this: for a 64 byte transaction, the bottom 6 bits of the address must be
+	// zero. These zero bits must be made up of the byte offset (3 bits) and also
+	// from the bottom bits of the column
+	//
+	// For example: cowLowBits = log2(64bytes) - 3 bits = 3 bits
+
+	begin_a >>= colLowBitWidth;
+	end_a >>= colLowBitWidth;
+	printf("Bit widths: chan:%d rank:%d bank:%d row:%d colLow:%d colHigh:%d off:%d Total:%d\n\n",channelBitWidth,rankBitWidth,bankBitWidth,rowBitWidth,colLowBitWidth
+				,colHighBitWidth,byteOffsetWidth,(channelBitWidth + rankBitWidth + bankBitWidth + rowBitWidth + colLowBitWidth + colHighBitWidth + byteOffsetWidth));
+
+
+}
+
+void address_mapping(UINT64 physicalAddress, int *chan, int *rank, int *bank, int *row, int *col, int addressMappingScheme)
+{
+	UINT64 tempA, tempB;
+	//perform various address mapping schemes
+	if (addressMappingScheme == 1)
+	{
+		//chan:rank:row:col:bank
+		tempA = physicalAddress;
+		physicalAddress = physicalAddress >> bankBitWidth;
+		tempB = physicalAddress << bankBitWidth;
+		*bank = tempA ^ tempB;
+
+		tempA = physicalAddress;
+		physicalAddress = physicalAddress >> colHighBitWidth;
+		tempB = physicalAddress << colHighBitWidth;
+		*col = tempA ^ tempB;
+		tempA = physicalAddress;
+		physicalAddress = physicalAddress >> rowBitWidth;
+		tempB = physicalAddress << rowBitWidth;
+		*row = tempA ^ tempB;
+
+		tempA = physicalAddress;
+		physicalAddress = physicalAddress >> rankBitWidth;
+		tempB = physicalAddress << rankBitWidth;
+		*rank = tempA ^ tempB;
+
+		tempA = physicalAddress;
+		physicalAddress = physicalAddress >> channelBitWidth;
+		tempB = physicalAddress << channelBitWidth;
+		*chan = tempA ^ tempB;
+
+	}
+	else if (addressMappingScheme == 2)
+	{
+		//*chan:*row:*col:*bank:*rank
+		tempA = physicalAddress;
+		physicalAddress = physicalAddress >> rankBitWidth;
+		tempB = physicalAddress << rankBitWidth;
+		*rank = tempA ^ tempB;
+
+		tempA = physicalAddress;
+		physicalAddress = physicalAddress >> bankBitWidth;
+		tempB = physicalAddress << bankBitWidth;
+		*bank = tempA ^ tempB;
+
+		tempA = physicalAddress;
+		physicalAddress = physicalAddress >> colHighBitWidth;
+		tempB = physicalAddress << colHighBitWidth;
+		*col = tempA ^ tempB;
+
+		tempA = physicalAddress;
+		physicalAddress = physicalAddress >> rowBitWidth;
+		tempB = physicalAddress << rowBitWidth;
+		*row = tempA ^ tempB;
+
+		tempA = physicalAddress;
+		physicalAddress = physicalAddress >> channelBitWidth;
+		tempB = physicalAddress << channelBitWidth;
+		*chan = tempA ^ tempB;
+
+	}
+	else if (addressMappingScheme == 3)
+	{
+		//*chan:*rank:*bank:*col:*row
+		tempA = physicalAddress;
+		physicalAddress = physicalAddress >> rowBitWidth;
+		tempB = physicalAddress << rowBitWidth;
+		*row = tempA ^ tempB;
+
+		tempA = physicalAddress;
+		physicalAddress = physicalAddress >> colHighBitWidth;
+		tempB = physicalAddress << colHighBitWidth;
+		*col = tempA ^ tempB;
+
+		tempA = physicalAddress;
+		physicalAddress = physicalAddress >> bankBitWidth;
+		tempB = physicalAddress << bankBitWidth;
+		*bank = tempA ^ tempB;
+
+		tempA = physicalAddress;
+		physicalAddress = physicalAddress >> rankBitWidth;
+		tempB = physicalAddress << rankBitWidth;
+		*rank = tempA ^ tempB;
+
+		tempA = physicalAddress;
+		physicalAddress = physicalAddress >> channelBitWidth;
+		tempB = physicalAddress << channelBitWidth;
+		*chan = tempA ^ tempB;
+
+	}
+	else if (addressMappingScheme == 4)
+	{
+		//*chan:*rank:*bank:*row:*col
+		tempA = physicalAddress;
+		physicalAddress = physicalAddress >> colHighBitWidth;
+		tempB = physicalAddress << colHighBitWidth;
+		*col = tempA ^ tempB;
+
+		tempA = physicalAddress;
+		physicalAddress = physicalAddress >> rowBitWidth;
+		tempB = physicalAddress << rowBitWidth;
+		*row = tempA ^ tempB;
+
+		tempA = physicalAddress;
+		physicalAddress = physicalAddress >> bankBitWidth;
+		tempB = physicalAddress << bankBitWidth;
+		*bank = tempA ^ tempB;
+
+		tempA = physicalAddress;
+		physicalAddress = physicalAddress >> rankBitWidth;
+		tempB = physicalAddress << rankBitWidth;
+		*rank = tempA ^ tempB;
+
+		tempA = physicalAddress;
+		physicalAddress = physicalAddress >> channelBitWidth;
+		tempB = physicalAddress << channelBitWidth;
+		*chan = tempA ^ tempB;
+
+	}
+	else if (addressMappingScheme == 5)
+	{
+		//*chan:*row:*col:*rank:*bank
+
+		tempA = physicalAddress;
+		physicalAddress = physicalAddress >> bankBitWidth;
+		tempB = physicalAddress << bankBitWidth;
+		*bank = tempA ^ tempB;
+
+		tempA = physicalAddress;
+		physicalAddress = physicalAddress >> rankBitWidth;
+		tempB = physicalAddress << rankBitWidth;
+		*rank = tempA ^ tempB;
+
+		tempA = physicalAddress;
+		physicalAddress = physicalAddress >> colHighBitWidth;
+		tempB = physicalAddress << colHighBitWidth;
+		*col = tempA ^ tempB;
+
+		tempA = physicalAddress;
+		physicalAddress = physicalAddress >> rowBitWidth;
+		tempB = physicalAddress << rowBitWidth;
+		*row = tempA ^ tempB;
+
+		tempA = physicalAddress;
+		physicalAddress = physicalAddress >> channelBitWidth;
+		tempB = physicalAddress << channelBitWidth;
+		*chan = tempA ^ tempB;
+
+
+	}
+	else if (addressMappingScheme == 6)
+	{
+		//*chan:*row:*bank:*rank:*col
+
+		tempA = physicalAddress;
+		physicalAddress = physicalAddress >> colHighBitWidth;
+		tempB = physicalAddress << colHighBitWidth;
+		*col = tempA ^ tempB;
+
+		tempA = physicalAddress;
+		physicalAddress = physicalAddress >> rankBitWidth;
+		tempB = physicalAddress << rankBitWidth;
+		*rank = tempA ^ tempB;
+
+		tempA = physicalAddress;
+		physicalAddress = physicalAddress >> bankBitWidth;
+		tempB = physicalAddress << bankBitWidth;
+		*bank = tempA ^ tempB;
+
+		tempA = physicalAddress;
+		physicalAddress = physicalAddress >> rowBitWidth;
+		tempB = physicalAddress << rowBitWidth;
+		*row = tempA ^ tempB;
+
+		tempA = physicalAddress;
+		physicalAddress = physicalAddress >> channelBitWidth;
+		tempB = physicalAddress << channelBitWidth;
+		*chan = tempA ^ tempB;
+
+
+	}
+	// clone of scheme 5, but channel moved to lower bits
+	else if (addressMappingScheme == 7)
+	{
+		//*row:*col:*rank:*bank:*chan
+		tempA = physicalAddress;
+		physicalAddress = physicalAddress >> channelBitWidth;
+		tempB = physicalAddress << channelBitWidth;
+		*chan = tempA ^ tempB;
+
+		tempA = physicalAddress;
+		physicalAddress = physicalAddress >> bankBitWidth;
+		tempB = physicalAddress << bankBitWidth;
+		*bank = tempA ^ tempB;
+
+		tempA = physicalAddress;
+		physicalAddress = physicalAddress >> rankBitWidth;
+		tempB = physicalAddress << rankBitWidth;
+		*rank = tempA ^ tempB;
+
+		tempA = physicalAddress;
+		physicalAddress = physicalAddress >> colHighBitWidth;
+		tempB = physicalAddress << colHighBitWidth;
+		*col = tempA ^ tempB;
+
+		tempA = physicalAddress;
+		physicalAddress = physicalAddress >> rowBitWidth;
+		tempB = physicalAddress << rowBitWidth;
+		*row = tempA ^ tempB;
+
+	}
 }
